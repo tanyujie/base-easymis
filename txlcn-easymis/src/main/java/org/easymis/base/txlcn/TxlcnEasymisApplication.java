69ad9e3a8d21b336372f1f3841eedf4ff9551b93
tanyujie <13551259347@139.com>
2019-05-26 19:43:08 +0800

    init

---
+++ txlcn-easymis/src/main/java/org/easymis/base/txlcn/TxlcnEasymisApplication.java
@@ -0,0 +1,13 @@
+package org.easymis.base.txlcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxlcnEasymisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxlcnEasymisApplication.class, args);
+	}
+
+}
---
+++ txlcn-easymis/src/test/java/org/easymis/base/txlcn/TxlcnEasymisApplicationTests.java
@@ -0,0 +1,16 @@
+package org.easymis.base.txlcn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TxlcnEasymisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
