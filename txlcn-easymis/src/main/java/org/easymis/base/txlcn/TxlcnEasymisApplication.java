9e593f1d20a27da8a38ae99428b07d0150b0f5ec
tanyujie <13551259347@139.com>
2019-05-26 20:50:01 +0800

    init

--- txlcn-easymis/src/main/java/org/easymis/base/txlcn/TxlcnEasymisApplication.java
+++ txlcn-easymis/src/main/java/org/easymis/base/txlcn/TxlcnEasymisApplication.java
@@ -8,7 +8,7 @@ import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 @SpringBootApplication
 @EnableTransactionManagerServer
 public class TxlcnEasymisApplication {
-
+//https://github.com/codingapi/txlcn-demo
 	public static void main(String[] args) {
 		SpringApplication.run(TxlcnEasymisApplication.class, args);
 	}
